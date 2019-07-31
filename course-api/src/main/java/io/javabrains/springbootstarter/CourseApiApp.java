2fc5d5abc695fce8cef6b70f154ec6eba1337460
10190 <10190@SKCC19N00562.SKCC.NET>
2019-07-31 14:08:21 +0900

    Run as Java Application

---
+++ course-api/src/main/java/io/javabrains/springbootstarter/CourseApiApp.java
@@ -0,0 +1,14 @@
+package io.javabrains.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //-> this class has the main method
+public class CourseApiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiApp.class, args ); 
+
+	}
+
+}
